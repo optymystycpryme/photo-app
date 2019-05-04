3a122df7cda3da10ab53ed201f62369b64814883
brian haltom <brianwh87@gmail.com>
2019-05-04 15:13:54 -0700

    Implemented carrierwave and carrierwave aws

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = Rails.application.credentials.production[:aws][:s3_bucket_name]
+  config.aws_acl    = 'private'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+  config.aws_attributes = -> { {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  } }
+
+  config.aws_credentials = {
+    access_key_id:     Rails.application.credentials.production[:aws][:access_key_id],
+    secret_access_key: Rails.application.credentials.production[:aws][:secret_access_key],
+    region:            Rails.application.credentials.production[:aws][:aws_region], # Required
+    stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+  }
+end
